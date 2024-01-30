9361fa4d30a3873185f557e9d9ac42301b112d3b
MBVNOG <114583371+MBVNOG@users.noreply.github.com>
2024-01-30 15:52:16 +0100

    Update PatchWinREScript_General.ps1
    fixing typo
    
    Signed-off-by: MBVNOG <114583371+MBVNOG@users.noreply.github.com>

--- PatchWinREScript_General.ps1
+++ PatchWinREScript_General.ps1
@@ -31,8 +31,8 @@ Param (
 # Help functions
 # ------------------------------------
 # Log message
-$logFile = ".\PatchWinREScript_2004plus.txt"
-Write-Output "Logfile for PatchWinREScript_2004plus.ps1" | Out-File $logFile
+$logFile = ".\PatchWinREScript_General.txt"
+Write-Output "Logfile for PatchWinREScript_General.ps1" | Out-File $logFile
 
 function LogMessage([string]$message)
 {
